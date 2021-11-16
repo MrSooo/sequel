27f4108de60cb9fbf2845883f135eb64af2b5c91
MrSooo <51949526+MrSooo@users.noreply.github.com>
2021-11-16 08:47:09 +0700

    +1st

---
+++ app/controllers/userController.js
@@ -0,0 +1,45 @@
+const db = require("../models");
+const User = db.users;
+const Op = db.Sequelize.Op;
+
+exports.create = (req, res) => {
+  // Validate request
+  if (!req.body.title) {
+    res.status(400).send({
+      message: "Content can not be empty!",
+    });
+    return;
+  }
+
+  // Create a User
+  const user = {
+    username: req.body.username,
+    password: req.body.password,
+  };
+
+  // Save User in the database
+  User.create(user)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating the User.",
+      });
+    });
+};
+
+exports.findAll = (req, res) => {
+  const title = req.query.title;
+  var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+
+  User.findAll({ where: condition })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving Users.",
+      });
+    });
+};
